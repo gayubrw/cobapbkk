60d351eafea5f889e614f41b851b9554662fb82b
Gayu Baruwa <144690276+gayubrw@users.noreply.github.com>
2024-09-01 02:44:03 +0700

    Update base.blade.php

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -179,9 +179,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
-                <img src="{{ asset('adminlte/dist/img/catharsis.jpg') }}" alt="AdminLTE Logo"
-                    class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">Catharsis Emprire</span>
+                <span class="brand-text font-weight-light">Tugas 1</span>
             </a>
 
             <!-- Sidebar -->
