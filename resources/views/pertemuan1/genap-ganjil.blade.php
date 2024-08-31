04f1785723e40ea8e5f94daf26f9cf3122ed9ec7
Gayu Baruwa <144690276+gayubrw@users.noreply.github.com>
2024-09-01 02:35:01 +0700

    Update genap-ganjil.blade.php

--- resources/views/pertemuan1/genap-ganjil.blade.php
+++ resources/views/pertemuan1/genap-ganjil.blade.php
@@ -4,36 +4,12 @@
 
 @section('content')
     <h1>Masukkan Angka</h1>
-    <div class="card">
-        <div class="card-header">
-            <div class="card-title">
-                Post Request
-            </div>
-        </div>
-        <div class="card-body">
-            <form action="#" method="POST">
-                @csrf
-                <label for="n">Enter a number (n):</label>
-                <input type="text" name="n" id="n" min="1" required>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    </div>
-
-    <div class="card">
-        <div class="card-header">
-            <div class="card-title">Get Request</div>
-        </div>
-        <div class="card-body">
             <form action="#" method="GET">
                 <label for="n">Enter a number (n):</label>
                 <input type="text" name="n" id="n" min="1" required>
                 <button type="submit">Submit</button>
             </form>
-        </div>
-    </div>
-
-
+            
     @if (count($numberDetails) > 0)
         <h2>Hasil</h2>
         <table border="1">
