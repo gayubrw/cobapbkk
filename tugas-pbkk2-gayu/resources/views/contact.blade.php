268ea23a3b3cd21be2f0bbeb77ff6c2fc8a2f3ae
Gayu Baruwa <144690276+gayubrw@users.noreply.github.com>
2024-09-08 14:25:27 +0700

    Delete tugas-pbkk2-gayu directory

--- tugas-pbkk2-gayu/resources/views/contact.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div style="color: white;">
-        <h1 style="font-size: 24px; font-weight: bold;">Contact Us</h1>
-        <p>Ini adalah halaman Contact Us.</p>
-    </div>
-@endsection
